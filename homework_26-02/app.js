a0fb7742ad405c86db4da709feebc3fd254f30ee
Grebenyuk Viktor <suhov1985@gmail.com>
2018-03-04 19:55:19 +0200

    frontend

---
+++ homework_26-02/app.js
@@ -0,0 +1,30 @@
+
+// Task1
+alert("hello"); // Приветствие
+// End Task1
+
+// Task2
+var firstName = 'Alex';
+var lastName = 0;
+lastName = firstName;
+document.write(lastName);
+// End Task2
+
+// Task3
+var name = 0;
+var surname = 0;
+var age = 0;
+var education = 0;
+var hobby = 0;
+// End Task3
+
+// Task4
+// Ничего не выведет так как будет ошибка на строке 3 - неизвестная переменная x;
+// End Task4
+
+// Task5
+var a = prompt("Num 1: ");
+var b = prompt("Num 2: ");
+var sum = parseInt(a) + parseInt(b);
+alert("Sum:" + sum);
+// End Task5
